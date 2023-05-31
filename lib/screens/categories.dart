0667a562e67cedd26ac48fdefbfe30206b59d1cf
Chiraag_Arun_Kumar <39455997+chiraag918@users.noreply.github.com>
2023-06-01 01:02:27 +0530

    Added inkwell on category widgets, setup meals screen and dummy data

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_food_app/data/dummy_data.dart';
 import 'package:flutter_food_app/widgets/category_item.dart';
 
---
+++ lib/screens/meals.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_food_app/models/meal.dart';
+
+class Meals extends StatelessWidget {
+  const Meals({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("");
+  }
+}
