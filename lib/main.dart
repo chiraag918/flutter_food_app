4317ab293e258c720b36d7f261fe89ba4e5dd57e
Chiraag_Arun_Kumar <39455997+chiraag918@users.noreply.github.com>
2023-05-31 23:41:48 +0530

    Setup dummy categories, category class & category item widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_food_app/categories.dart';
+import 'package:flutter_food_app/screens/categories.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({super.key, required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
